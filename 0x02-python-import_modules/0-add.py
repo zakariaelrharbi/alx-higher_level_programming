11a4bbc3f837a654bfed2bb137177ce0e6682e8e
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2023-11-03 00:40:10 +0100

    import module

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from add_0 import add
+
+# """My addition function
+# Args:
+# a: first integer
+# b: second integer
+# Returns:
+#   The return value. a + b
+# """
+
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
