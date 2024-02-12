d2dbea04ee824a7c969b3b17888eaa14e8e18828
zakariaelrharbi <zakariaelrharbi269@gmail.com>
2024-02-13 00:08:23 +0100

    arguments done

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const process = require('process');
+
+if (process.argv[3]) {
+    console.log('Arguments found');
+}
+else if (process.argv[2]) {
+    console.log('Argument found');
+} else {
+    console.log('No argument');
+}
